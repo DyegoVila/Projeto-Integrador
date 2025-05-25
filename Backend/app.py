f60e7868ef08fbb1ca95b44633310ce6aec62261
emanuel120021 <emanuel.lazaro.52@gmail.com>
2025-05-25 13:30:48 -0300

    Formulário de login do refator: Altere os nomes dos campos de entrada de 'senha' para 'senha' e atualize as referências JavaScript relacionadas para obter consistência

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request,  session, jsonify
 import sqlite3
 from flask_cors import CORS
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = 'chave-secreta'
-CORS(app)  # Libera CORS para todas as rotas e origens
+CORS(app, supports_credentials=True)  # Libera CORS para todas as rotas e origens
 
 
 def get_db_connection():
@@ -15,32 +16,36 @@ def get_db_connection():
 # Login: recebe JSON { "usuario": "...", "senha": "..." }
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.json
-    usuario = data.get('usuario')
-    senha = data.get('senha')
+    data = request.get_json()
+    email = data.get('email')
+    senha = hash_senha(data.get('senha')) 
 
     conn = get_db_connection()
-    cursor = conn.execute('SELECT id, login FROM usuarios WHERE login = ? AND senha = ?', (usuario, senha))
+    cursor = conn.execute('SELECT id, nome, login, cargo FROM usuarios WHERE login = ? AND senha = ?', (email, senha))
     user = cursor.fetchone()
     conn.close()
 
+    print(dict(user))
+
+
     # Aqui você pode validar no banco ou deixar fixo como antes
     if user:
-        session['usuario'] = usuario
-        return jsonify({'status': 'sucesso', 'id': user['id'], 'username': user['username']}), 200
+        session['login'] = email
+        return jsonify({'status': True, 'usuario': user['nome'], 'cargo': user['cargo'] }), 200
     else:
-        return jsonify({'status': 'erro', 'mensagem': 'Usuário ou senha inválidos'}), 401
+        return jsonify({'status': False, 'mensagem': 'Usuário ou senha inválidos'}), 401
 
 # Cadastro: recebe JSON { "username": "...", "password": "..." }
 @app.route('/cadastro', methods=['POST'])
 def cadastro():
     data = request.json
-    usuario = data.get('username')
-    senha = data.get('password')
+    nome = data.get('nome')
+    login = data.get('login')
+    senha = data.get('senha')
 
     conn = get_db_connection()
     try:
-        conn.execute('INSERT INTO usuarios (username, password) VALUES (?, ?)', (usuario, senha))
+        conn.execute('INSERT INTO usuarios (nome, login, senha) VALUES (?, ?, ?)', (nome, login, senha))
         conn.commit()
         return jsonify({"message": "Usuário cadastrado com sucesso!"}), 201
     except sqlite3.IntegrityError:
@@ -52,7 +57,7 @@ def cadastro():
 @app.route('/users', methods=['GET'])
 def users():
     conn = get_db_connection()
-    usuarios = conn.execute('SELECT id, username FROM usuarios').fetchall()
+    usuarios = conn.execute('SELECT id, nome FROM usuarios').fetchall()
     conn.close()
     resultado = [dict(usuario) for usuario in usuarios]
     return jsonify(resultado)
@@ -62,6 +67,11 @@ def users():
 def logout():
     session.pop('usuario', None)
     return jsonify({"message": "Logout efetuado"}), 200
+
+
+def hash_senha(senha):
+    return hashlib.md5(senha.encode()).hexdigest()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
